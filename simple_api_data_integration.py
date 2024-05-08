ab630cbe30d8b140aef1d9b07065d3ee8d7ed46d
Wiza <64467592+wizaphiri@users.noreply.github.com>
2024-05-08 11:46:20 +0200

    Update simple_api_data_integration.py

--- simple_api_data_integration.py
+++ simple_api_data_integration.py
@@ -33,15 +33,28 @@ r = requests.get(
 )
 
 df = pd.read_csv(BytesIO(r.content))
-
+df_records = df.shape[0]
+print(datetime.now().strftime("%Y-%m-%d %H:%M:%S")+': '+str(df_records)+' records extracted')
 # export df to csv
 # print(df.info())
 
-engine = create_engine('postgresql+psycopg2://user:password@localhost/db1')
+engine = create_engine('postgresql+psycopg2://userx:passx@localhost/dbx')
 with engine.begin() as conn:
-    conn.exec_driver_sql("DROP TABLE IF EXISTS integration_res")
-    #print(datetime.now().strftime("%Y-%m-%d %H:%M:%S")+': table integration_res dropped')
+    conn.exec_driver_sql("DROP TABLE IF EXISTS art_scanform")
+    #print(datetime.now().strftime("%Y-%m-%d %H:%M:%S")+': table art_scanform dropped')
 
-df.to_sql('integration_res', engine, if_exists='append')
-print(datetime.now().strftime("%Y-%m-%d %H:%M:%S")+': Data integration complete')
+df.to_sql('art_scanform', engine, if_exists='append')
+
+table = 'art_scanform'
+
+with engine.connect() as connection:
+    count_statement = 'select count(*) from art_scanform'
+    record_count = connection.execute(count_statement).scalar()
+
+print(datetime.now().strftime("%Y-%m-%d %H:%M:%S")+': '+str(record_count)+' records loaded')
+if df_records == record_count:
+    print(datetime.now().strftime("%Y-%m-%d %H:%M:%S")+': Scanform data integration completed successfully')
+else:
+    print(datetime.now().strftime("%Y-%m-%d %H:%M:%S")+': etl mismatch - extracted '+str(df_records)+' records and loaded '+str(record_count)+' records')
+    print(datetime.now().strftime("%Y-%m-%d %H:%M:%S")+': Scanform data integration completed with errors')
 
